0c4598fad7266c9ac3c6b32156039cff1c9654f6
hrrocha0 <99548553+hrrocha0@users.noreply.github.com>
2023-11-29 09:08:29 -0300

    leitura da entrada

--- main.cpp
+++ main.cpp
@@ -1,9 +1,41 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
 #include "IOManager.cpp"
 #include "need.h"
 
-int main() {
-  // abrir as instancias dos gerenciadores
-  // fazer tipo um kernel ?
+std::vector<std::string> read_lines(const std::string& path)
+{
+	std::vector<std::string> lines;
+	std::string line;
+	std::ifstream file(path);
 
+	if (file.fail())
+	{
+		// Incluir mensagem de erro
+		abort();
+	}
+	while (std::getline(file, line))
+	{
+		lines.push_back(line);
+	}
+	file.close();
 
+	return lines;
+}
+
+int main(int argc, char* argv[])
+{
+	// abrir as instancias dos gerenciadores
+	// fazer tipo um kernel ?
+	if (argc != 3)
+	{
+		// Incluir mensagem de erro
+		abort();
+	}
+	std::string processes_path = argv[1];
+	std::string files_path = argv[2];
+
+	std::vector<std::string> processes = read_lines(processes_path);
+	std::vector<std::string> files = read_lines(files_path);
 }
